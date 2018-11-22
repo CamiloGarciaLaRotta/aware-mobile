282314af05bf919baa1cf9e5ef8ad4f21c39a50e
Jay Abi-Saad <jayabisaad@gmail.com>
2018-11-21 21:13:06 -0500

    working ios cam

---
+++ awareMobileFE/Camera.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Dimensions, Alert, StyleSheet, ActivityIndicator } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+
+export default class Camera extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = { 
+            identifedAs: '',
+            loading: false
+        }
+    }
+
+    takePicture = async function(){
+
+        if (this.camera) {
+
+            // Pause the camera's preview
+            this.camera.pausePreview();
+
+            // Set the activity indicator
+            this.setState((previousState, props) => ({
+                loading: true
+            }));
+
+            // Set options
+            const options = {
+                base64: true
+            };
+
+            // Get the base64 version of the image
+            const data = await this.camera.takePictureAsync(options)
+
+        }
+    }
+
+    render() {
+        return (
+            <RNCamera ref={ref => {this.camera = ref;}} style={styles.preview}>
+            <ActivityIndicator size="large" style={styles.loadingIndicator} color="#fff" animating={this.state.loading}/>
+            </RNCamera>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width,
+    },
+    loadingIndicator: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
